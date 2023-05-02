ed54135a0cde6de4079df70bc27c5883a1efeecc
Bwambocos <babcs2035@gmail.com>
2023-05-02 13:27:55 +0900

    [add] debug func

--- ITC-LMS.py
+++ ITC-LMS.py
@@ -9,6 +9,7 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from apscheduler.schedulers.blocking import BlockingScheduler
+import argparse
 
 sched = BlockingScheduler(
     executors={
@@ -109,3 +110,18 @@ def scheduled_job():
 
 sched.start()
 print("ITC-LMS: Bot initialized")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("function_name", type=str, help="set fuction name in this file")
+    parser.add_argument(
+        "-i", "--func_args", nargs="*", help="args in function", default=[]
+    )
+    args = parser.parse_args()
+
+    # このファイル内の関数を取得
+    func_dict = {k: v for k, v in locals().items() if callable(v)}
+    # 引数のうち，数値として解釈できる要素はfloatにcastする
+    func_args = [float(x) if x.isnumeric() else x for x in args.func_args]
+    # 関数実行
+    ret = func_dict[args.function_name](*func_args)
