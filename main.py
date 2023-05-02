81aab1ae3437d8057936040f3ef27e6c8f79ca4e
Bwambocos <babcs2035@gmail.com>
2023-05-02 14:36:13 +0900

    [fix] call LMS

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 import status
 from dotenv import load_dotenv
+
 load_dotenv()
 
 sched = BlockingScheduler(
     executors={
-        'threadpool': ThreadPoolExecutor(max_workers=1),
-        'processpool': ProcessPoolExecutor(max_workers=1)
+        "threadpool": ThreadPoolExecutor(max_workers=1),
+        "processpool": ProcessPoolExecutor(max_workers=1),
     }
 )
 
@@ -22,7 +23,7 @@ def scheduled_job():
     print("----- sendStatus done -----")
 
 
-subprocess.Popen(["python3", "-u", "ITC-LMS.py"])
+subprocess.Popen(["python3", "-u", "LMS.py"])
 
 sched.start()
 print("main: Bot initialized")
